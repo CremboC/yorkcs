a255df12ff16d51345647be4108060ded2239204
P Imbrasas <pi514@york.ac.uk>
2014-02-13 10:11:16 +0000

    POPL Pascal Exercise 6 update

--- POPL/Concurrency/p6.pas
+++ POPL/Concurrency/p6.pas
@@ -1,17 +1,54 @@
 program barrier;
 
+var mutex : condition;
+
 monitor Barrier;
     export
-        wait, release;
+        Wait, Release;
     var
         okToRelease : condition;
 
-    procedure wait;
+    procedure Wait;
     begin
-
+        delay(okToRelease);
     end;
 
-    procedure release;
+    procedure Release;
     begin
-
+        while not empty(okToRelease) do
+            resume(okToRelease);
     end;
+end;
+
+process type Caller(id : char);
+begin
+
+    writeln('Caller ', id, ' calling.');
+
+    Barrier.wait;
+
+end;
+
+process Releaser;
+begin
+
+    Barrier.release;
+
+end;
+
+var C1, C2, C3, C4, C5 : Caller;
+
+begin
+
+    initial(mutex);
+
+    cobegin
+        C1;
+        C2;
+        C3;
+        C4;
+        C5;
+        Releaser;
+    coend;
+
+end.
