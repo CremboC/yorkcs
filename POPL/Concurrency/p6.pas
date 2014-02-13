13bd87ad9668892f65f22c0ee53bb4a2ace371c0
P Imbrasas <pi514@york.ac.uk>
2014-02-13 10:31:10 +0000

    SYAC Pascal Exercise 6 done? Updated makefile

--- POPL/Concurrency/p6.pas
+++ POPL/Concurrency/p6.pas
@@ -1,53 +1,83 @@
-program barrier;
+program exc6;
 
-var mutex : condition;
+var mutex : semaphore;
 
 monitor Barrier;
     export
         Wait, Release;
     var
+        arrived : integer;
         okToRelease : condition;
+        releaseOccured : boolean;
 
     procedure Wait;
     begin
+        arrived := arrived + 1;
         delay(okToRelease);
     end;
 
     procedure Release;
     begin
-        while not empty(okToRelease) do
-            resume(okToRelease);
+        if (arrived = 5) or (releaseOccured = true) then
+        begin
+            releaseOccured := true;
+
+            while not empty(okToRelease) do
+            begin
+                resume(okToRelease);
+                arrived := arrived - 1;
+            end;
+
+            if arrived = 0 then
+            begin
+                releaseOccured := false;
+            end;
+        end;
     end;
 end;
 
 process type Caller(id : char);
 begin
 
-    writeln('Caller ', id, ' calling.');
+    wait(mutex);
 
-    Barrier.wait;
+    writeln('Caller ', id, ' waiting.');
+    writeln;
+
+    signal(mutex);
+
+    Barrier.Wait;
+
+    wait(mutex);
+
+    writeln('Caller ', id, ' calling.');
+    writeln;
+
+    signal(mutex);
 
 end;
 
 process Releaser;
 begin
+    repeat
 
-    Barrier.release;
+    Barrier.Release;
 
+    forever;
 end;
 
 var C1, C2, C3, C4, C5 : Caller;
 
 begin
 
-    initial(mutex);
+    initial(mutex, 1);
 
     cobegin
-        C1;
-        C2;
-        C3;
-        C4;
-        C5;
+        C1('a');
+        C2('b');
+        C3('c');
+        C4('d');
+        C5('e');
         Releaser;
     coend;
 
