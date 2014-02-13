1c7bfdcb375936fe117fae13109c2343807fc7f0
P Imbrasas <pi514@york.ac.uk>
2014-02-13 09:58:42 +0000

    Testing committing from Sublime

--- POPL/Concurrency/p6.pas
+++ POPL/Concurrency/p6.pas
@@ -1,1 +1,17 @@
 program barrier;
+
+monitor Barrier;
+    export
+        wait, release;
+    var
+        okToRelease : condition;
+
+    procedure wait;
+    begin
+
+    end;
+
+    procedure release;
+    begin
+
+    end;
